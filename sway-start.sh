ff581d2c56e19d2a9dbc8995288221dcae034ce5
vkuzel <vkuzel@gmail.com>
2025-02-27 16:06:24 +0100

    Disable atomic mode-setting for wlroot.

--- sway-start.sh
+++ sway-start.sh
@@ -8,4 +8,8 @@ export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
 # Xwayland compatibility
 export XWAYLAND_NO_GLAMOR=1
 
+# Disable atomic mode-setting for wlroots
+# Avoid "Failed to pick cursor plane format" error
+export WLR_DRM_NO_ATOMIC=1
+
 "$work_dir/sway/build/sway/sway" --unsupported-gpu
